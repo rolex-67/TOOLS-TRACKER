b4dd8e2c32c013cfa1fa7af42527f72af1607458
Nipun Dewangan <nipundewangan4@gmail.com>
2025-07-11 13:09:28 +0530

    Delete final-app directory

--- final-app/final-app/backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['Admin', 'User', 'Toolskeeper'], default: 'User' }
-});
-
-module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
